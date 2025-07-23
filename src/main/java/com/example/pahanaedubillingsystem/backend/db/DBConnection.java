c6008dbc3406d6d93ce3ba0fca57ecad6171777c
Pulinda Mathagadeera <122967894+Puliya99@users.noreply.github.com>
2025-07-24 05:19:52 +0530

    create DBConnection

---
+++ src/main/java/com/example/pahanaedubillingsystem/backend/db/DBConnection.java
@@ -0,0 +1,29 @@
+package com.example.pahanaedubillingsystem.backend.db;
+
+
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+import java.sql.Connection;
+
+public class DBConnection {
+    private static DBConnection dbConnection;
+    private Connection connection;
+
+    private DBConnection() {
+        try {
+            InitialContext context = new InitialContext();
+            DataSource lookup = (DataSource) context.lookup("java:comp/env/jdbc/pahanaedu");
+            this.connection = lookup.getConnection();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static DBConnection getInstance() {
+        return dbConnection == null ? dbConnection = new DBConnection() : dbConnection;
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+}
