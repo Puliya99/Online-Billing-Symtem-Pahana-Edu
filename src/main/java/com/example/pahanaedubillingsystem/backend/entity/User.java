6d585a47d0dd1959ee7203e83fa5375a512adba0
Pulinda Mathagadeera <122967894+Puliya99@users.noreply.github.com>
2025-08-06 20:01:06 +0530

    replace Lombok annotations in User entity with explicit getter, setter, and constructor implementations

--- src/main/java/com/example/pahanaedubillingsystem/backend/entity/User.java
+++ src/main/java/com/example/pahanaedubillingsystem/backend/entity/User.java
@@ -1,13 +1,30 @@
 package com.example.pahanaedubillingsystem.backend.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class User {
     private String username;
     private String password;
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public User() {
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
