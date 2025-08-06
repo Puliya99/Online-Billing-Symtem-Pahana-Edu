104c3bd9d2f0c6640483f711ceb189b166f4d2b0
Pulinda Mathagadeera <122967894+Puliya99@users.noreply.github.com>
2025-08-06 20:02:25 +0530

    replace Lombok annotations in Customer entity with explicit getter, setter, and constructor implementations

--- src/main/java/com/example/pahanaedubillingsystem/backend/entity/Customer.java
+++ src/main/java/com/example/pahanaedubillingsystem/backend/entity/Customer.java
@@ -1,18 +1,61 @@
 package com.example.pahanaedubillingsystem.backend.entity;
 
-
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class Customer {
     private String accountNo;
     private String name;
     private String address;
     private String telephone;
     private int unitsConsumed;
+
+    public Customer(String accountNo, String name, String address, String telephone, int unitsConsumed) {
+        this.accountNo = accountNo;
+        this.name = name;
+        this.address = address;
+        this.telephone = telephone;
+        this.unitsConsumed = unitsConsumed;
+    }
+
+    public Customer() {
+    }
+
+    public String getAccountNo() {
+        return accountNo;
+    }
+
+    public void setAccountNo(String accountNo) {
+        this.accountNo = accountNo;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public int getUnitsConsumed() {
+        return unitsConsumed;
+    }
+
+    public void setUnitsConsumed(int unitsConsumed) {
+        this.unitsConsumed = unitsConsumed;
+    }
 }
 
