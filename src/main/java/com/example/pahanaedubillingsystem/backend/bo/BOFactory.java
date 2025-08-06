af957f8effd583369c8522a905d5d91c3de0d86f
Pulinda Mathagadeera <122967894+Puliya99@users.noreply.github.com>
2025-08-07 05:25:08 +0530

    implement UserBOImpl and BOFactory classes for user-related business operations

---
+++ src/main/java/com/example/pahanaedubillingsystem/backend/bo/BOFactory.java
@@ -0,0 +1,27 @@
+package com.example.pahanaedubillingsystem.backend.bo;
+
+import com.example.pahanaedubillingsystem.backend.bo.custom.impl.*;
+
+public class BOFactory {
+    private static BOFactory boFactory;
+
+    private BOFactory() {
+    }
+
+    public static BOFactory getBoFactory() {
+        return (boFactory == null) ? boFactory = new BOFactory() : boFactory;
+    }
+
+    public enum BOTypes {
+        CUSTOMER, ITEM, BILL, USER
+    }
+
+    public SuperBO getBO(BOTypes types) {
+        switch (types) {
+            case USER:
+                return new UserBOImpl();
+            default:
+                return null;
+        }
+    }
+}
