39cd50decf9c1d09de9ef66078e2afbac70418d3
Pulinda Mathagadeera <122967894+Puliya99@users.noreply.github.com>
2025-08-06 19:58:40 +0530

    update UserDTO to replace Lombok annotations with explicit getter, setter, and constructor implementations

--- src/main/java/com/example/pahanaedubillingsystem/backend/dto/UserDTO.java
+++ src/main/java/com/example/pahanaedubillingsystem/backend/dto/UserDTO.java
@@ -1,13 +1,30 @@
 package com.example.pahanaedubillingsystem.backend.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class UserDTO {
     private String username;
     private String password;
+
+    public UserDTO(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public UserDTO() {
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
