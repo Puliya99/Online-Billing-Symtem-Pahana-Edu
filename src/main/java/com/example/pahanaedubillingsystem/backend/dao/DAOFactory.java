50f193a5aeae73ede5407371426600170d8d3762
Pulinda Mathagadeera <122967894+Puliya99@users.noreply.github.com>
2025-08-06 19:22:43 +0530

    add DAOFactory and CustomerDAOImpl with CRUD operations and customer-bill deletion logic

---
+++ src/main/java/com/example/pahanaedubillingsystem/backend/dao/DAOFactory.java
@@ -0,0 +1,27 @@
+package com.example.pahanaedubillingsystem.backend.dao;
+
+import com.example.pahanaedubillingsystem.backend.dao.custom.impl.*;
+
+public class DAOFactory {
+    private static DAOFactory daoFactory;
+
+    private DAOFactory() {
+    }
+
+    public static DAOFactory getDaoFactory() {
+        return (daoFactory == null) ? daoFactory = new DAOFactory() : daoFactory;
+    }
+
+    public enum DAOTypes {
+        CUSTOMER, ITEM, BILL, USER
+    }
+
+    public SuperDAO getDAO(DAOTypes types) {
+        switch (types) {
+            case CUSTOMER:
+                return new CustomerDAOImpl();
+            default:
+                return null;
+        }
+    }
+}
