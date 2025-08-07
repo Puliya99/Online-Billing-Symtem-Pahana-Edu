91df267248ae4369659c8be5d04f815dcec2a2d6
Pulinda Mathagadeera <122967894+Puliya99@users.noreply.github.com>
2025-08-07 05:54:29 +0530

    replace `CrudUtil` with `UserBO` in UserController for user validation

--- src/main/java/com/example/pahanaedubillingsystem/backend/controller/UserController.java
+++ src/main/java/com/example/pahanaedubillingsystem/backend/controller/UserController.java
@@ -1,14 +1,14 @@
 package com.example.pahanaedubillingsystem.backend.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.example.pahanaedubillingsystem.backend.bo.BOFactory;
+import com.example.pahanaedubillingsystem.backend.bo.custom.UserBO;
+import com.example.pahanaedubillingsystem.backend.dto.UserDTO;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import com.example.pahanaedubillingsystem.backend.dto.UserDTO;
-import com.example.pahanaedubillingsystem.backend.util.CrudUtil;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -17,13 +17,14 @@ import java.io.IOException;
 @WebServlet(name = "UserModel", urlPatterns = "/UserModel", loadOnStartup = 4)
 public class UserController extends HttpServlet {
     private final static Logger logger = LoggerFactory.getLogger(UserController.class);
-    private ObjectMapper objectMapper = new ObjectMapper();
+    private final ObjectMapper objectMapper = new ObjectMapper();
+    private final UserBO userBO = (UserBO) BOFactory.getBoFactory().getBO(BOFactory.BOTypes.USER);
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         try {
             UserDTO user = objectMapper.readValue(req.getInputStream(), UserDTO.class);
-            boolean isValid = CrudUtil.getInstance().validateUser(user.getUsername(), user.getPassword());
+            boolean isValid = userBO.validateUser(user.getUsername(), user.getPassword());
             resp.getWriter().write(isValid ? "valid" : "invalid");
             logger.info(isValid ? "User Authenticated" : "User Authentication Failed");
         } catch (Exception e) {
