fab66df50c758b29bbe8193a1f86df7c8b6f9a5a
Pulinda Mathagadeera <122967894+Puliya99@users.noreply.github.com>
2025-08-13 22:23:34 +0530

    feat: implement VendorBOImpl with CRUD operations and integrate into BOFactory

---
+++ src/main/java/com/example/pahanaedubillingsystem/backend/bo/custom/impl/VendorBOImpl.java
@@ -0,0 +1,61 @@
+package com.example.pahanaedubillingsystem.backend.bo.custom.impl;
+
+import com.example.pahanaedubillingsystem.backend.bo.custom.VendorBO;
+import com.example.pahanaedubillingsystem.backend.dao.DAOFactory;
+import com.example.pahanaedubillingsystem.backend.dao.custom.VendorDAO;
+import com.example.pahanaedubillingsystem.backend.dto.VendorDTO;
+import com.example.pahanaedubillingsystem.backend.entity.Vendor;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class VendorBOImpl implements VendorBO {
+    private final VendorDAO vendorDAO = (VendorDAO) DAOFactory.getDaoFactory().getDAO(DAOFactory.DAOTypes.VENDOR);
+    @Override
+    public boolean saveVendor(VendorDTO dto) throws SQLException {
+        return vendorDAO.save(new Vendor(dto.getGrnId(), dto.getName(), dto.getItemId(), dto.getDescription(), dto.getQty(), dto.getBuyingPrice()));
+    }
+
+    @Override
+    public boolean updateVendor(VendorDTO dto) throws SQLException {
+        return vendorDAO.update(new Vendor(dto.getGrnId(), dto.getName(), dto.getItemId(), dto.getDescription(), dto.getQty(), dto.getBuyingPrice()));
+    }
+
+    @Override
+    public boolean deleteVendor(String grnId) throws SQLException {
+        return vendorDAO.delete(grnId);
+    }
+
+    @Override
+    public VendorDTO searchVendor(String grnId) throws SQLException {
+        Vendor vendor = vendorDAO.search(grnId);
+        if (vendor != null) {
+            return new VendorDTO(vendor.getGrnId(), vendor.getName(), vendor.getItemId(), vendor.getDescription(), vendor.getQty(), vendor.getBuyingPrice());
+        }
+        return null;
+    }
+
+    @Override
+    public List<VendorDTO> getAllVendors() throws SQLException {
+        List<VendorDTO> vendors = new ArrayList<>();
+        List<Vendor> all = vendorDAO.getAll();
+        for (Vendor v : all) {
+            vendors.add(new VendorDTO(v.getGrnId(), v.getName(), v.getItemId(), v.getDescription(), v.getQty(), v.getBuyingPrice()));
+        }
+        return vendors;
+    }
+
+    @Override
+    public List<String> getVendorIds() throws SQLException {
+        return vendorDAO.getIds();
+    }
+
+    @Override
+    public VendorDTO searchByIdVendor(String grnId) throws SQLException {
+        Vendor vendor = vendorDAO.searchById(grnId);
+        if (vendor != null) {
+            return new VendorDTO(vendor.getGrnId(), vendor.getName(), vendor.getItemId(), vendor.getDescription(), vendor.getQty(), vendor.getBuyingPrice());
+        }
+        return null;
+    }
+}
