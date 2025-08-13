5844600312c42ced1021d482009bb35ee32d9990
Pulinda Mathagadeera <122967894+Puliya99@users.noreply.github.com>
2025-08-13 21:06:54 +0530

    feat: add VendorDTO with fields, constructors, getters, setters, and toString method

---
+++ src/main/java/com/example/pahanaedubillingsystem/backend/dto/VendorDTO.java
@@ -0,0 +1,82 @@
+package com.example.pahanaedubillingsystem.backend.dto;
+
+public class VendorDTO {
+    private String grnId;
+    private String name;
+    private String itemId;
+    private String description;
+    private int qty;
+    private double buyingPrice;
+
+    public VendorDTO(String grnId, String name, String itemId, String description, int qty, double buyingPrice) {
+        this.grnId = grnId;
+        this.name = name;
+        this.itemId = itemId;
+        this.description = description;
+        this.qty = qty;
+        this.buyingPrice = buyingPrice;
+    }
+
+    public VendorDTO() {
+    }
+
+    public String getGrnId() {
+        return grnId;
+    }
+
+    public void setGrnId(String grnId) {
+        this.grnId = grnId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getItemId() {
+        return itemId;
+    }
+
+    public void setItemId(String itemId) {
+        this.itemId = itemId;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getQty() {
+        return qty;
+    }
+
+    public void setQty(int qty) {
+        this.qty = qty;
+    }
+
+    public double getBuyingPrice() {
+        return buyingPrice;
+    }
+
+    public void setBuyingPrice(double buyingPrice) {
+        this.buyingPrice = buyingPrice;
+    }
+
+    @Override
+    public String toString() {
+        return "VendorDTO{" +
+                "grnId='" + grnId + '\'' +
+                ", name='" + name + '\'' +
+                ", itemId='" + itemId + '\'' +
+                ", description='" + description + '\'' +
+                ", qty=" + qty +
+                ", buyingPrice=" + buyingPrice +
+                '}';
+    }
+}
