d9e5383e0b522ade146a64524ba516b5b98b6231
Pulinda Mathagadeera <122967894+Puliya99@users.noreply.github.com>
2025-08-16 21:00:43 +0530

    feat: update VendorDAO to include `grnDate` in operations
    - Modified SQL queries to handle the new `grn_date` field in the `vendors` table.
    - Updated `save`, `update`, and `search` methods to incorporate `grnDate`.
    - Refactored result mapping to initialize `Vendor` objects with `grnDate`.

--- src/main/java/com/example/pahanaedubillingsystem/backend/dao/custom/impl/VendorDAOImpl.java
+++ src/main/java/com/example/pahanaedubillingsystem/backend/dao/custom/impl/VendorDAOImpl.java
@@ -12,18 +12,19 @@ import java.util.List;
 public class VendorDAOImpl implements VendorDAO {
 
     private static final String GET_VENDORS = "SELECT * FROM vendors";
-    private static final String SAVE_VENDORS = "INSERT INTO vendors (grn_id, name, item_id, description, qty, buying_price) VALUES (?, ?, ?, ?, ?, ?)";
-    private static final String UPDATE_VENDORS = "UPDATE vendors SET name = ?, item_id = ?, description = ?, qty = ?, buying_price = ? WHERE grn_id = ?";
+    private static final String SAVE_VENDORS = "INSERT INTO vendors (grn_id, grn_date, name, item_id, description, qty, buying_price) VALUES (?, ?, ?, ?, ?, ?, ?)";
+    private static final String UPDATE_VENDORS = "UPDATE vendors SET grn_date = ?, name = ?, item_id = ?, description = ?, qty = ?, buying_price = ? WHERE grn_id = ?";
     private static final String DELETE_VENDORS = "DELETE FROM vendors WHERE grn_id = ?";
 
     @Override
     public boolean save(Vendor entity) throws SQLException {
-        return SQLUtil.execute(SAVE_VENDORS, entity.getGrnId(), entity.getName(), entity.getItemId(), entity.getDescription(), entity.getQty(), entity.getBuyingPrice());
+        return SQLUtil.execute(SAVE_VENDORS, entity.getGrnId(), entity.getGrnDate(), entity.getName(), entity.getItemId(), entity.getDescription(), entity.getQty(), entity.getBuyingPrice());
     }
 
     @Override
     public boolean update(Vendor entity) throws SQLException {
         return SQLUtil.execute(UPDATE_VENDORS,
+                entity.getGrnDate(),
                 entity.getName(),
                 entity.getItemId(),
                 entity.getDescription(),
@@ -41,7 +42,7 @@ public class VendorDAOImpl implements VendorDAO {
     public Vendor search(String grnId) throws SQLException {
         ResultSet rst = SQLUtil.execute("SELECT * FROM vendors WHERE grn_id = ?", grnId);
         if (rst.next()) {
-            return new Vendor(rst.getString("grn_id"), rst.getString("name"), rst.getString("item_id"), rst.getString("description"), rst.getInt("qty"), rst.getDouble("buying_price"));
+            return new Vendor(rst.getString("grn_id"), rst.getDate("grn_date"), rst.getString("name"), rst.getString("item_id"), rst.getString("description"), rst.getInt("qty"), rst.getDouble("buying_price"));
         }
         return null;
     }
@@ -51,7 +52,7 @@ public class VendorDAOImpl implements VendorDAO {
         List<Vendor> vendors = new ArrayList<>();
         ResultSet rst = SQLUtil.execute(GET_VENDORS);
         while (rst.next()) {
-            vendors.add(new Vendor(rst.getString("grn_id"), rst.getString("name"), rst.getString("item_id"), rst.getString("description"), rst.getInt("qty"), rst.getDouble("buying_price")));
+            vendors.add(new Vendor(rst.getString("grn_id"), rst.getDate("grn_date"), rst.getString("name"), rst.getString("item_id"), rst.getString("description"), rst.getInt("qty"), rst.getDouble("buying_price")));
         }
         return vendors;
     }
@@ -70,7 +71,7 @@ public class VendorDAOImpl implements VendorDAO {
     public Vendor searchById(String grnId) throws SQLException {
         ResultSet rst = SQLUtil.execute("SELECT * FROM vendors WHERE grn_id = ?", grnId);
         if (rst.next()) {
-            return new Vendor(rst.getString("grn_id"), rst.getString("name"), rst.getString("item_id"), rst.getString("description"), rst.getInt("qty"), rst.getDouble("buying_price"));
+            return new Vendor(rst.getString("grn_id"), rst.getDate("grn_date"), rst.getString("name"), rst.getString("item_id"), rst.getString("description"), rst.getInt("qty"), rst.getDouble("buying_price"));
         }
         return null;
     }
